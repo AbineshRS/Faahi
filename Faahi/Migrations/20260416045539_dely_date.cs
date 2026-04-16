7958406df5cd89b20862792db32e16041976f5b2
AbineshRS <abiabinesh483@gmail.com>
2026-04-16 10:03:28 +0500

    delevery_date added

---
+++ Faahi/Migrations/20260416045539_dely_date.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Faahi.Migrations
+{
+    /// <inheritdoc />
+    public partial class dely_date : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "delevery_date",
+                table: "om_CustomerOrders",
+                type: "datetime",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "delevery_date",
+                table: "om_CustomerOrders");
+        }
+    }
+}
